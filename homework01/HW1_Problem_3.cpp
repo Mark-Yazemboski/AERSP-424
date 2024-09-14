2862e4951a82290e26eeffd9ec789d96c7741e22
Mark-Yazemboski <PSSAChamp@gmail.com>
2024-09-14 16:16:01 -0400

    Finished Problem 4 and updated problem 3

--- homework01/HW1_Problem_3.cpp
+++ homework01/HW1_Problem_3.cpp
@@ -1,3 +1,4 @@
+//Imports all of the nessesary libraries
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -6,61 +7,77 @@
 #include <map>
 using namespace std;
 
+//Plane class
 class plane
 {
 
+    //Sets the containers for the to and from and distance data
     using Airport_Rout = std::pair<std::string, std::string>;
     using Flight_Distances = std::map<Airport_Rout, int>;
 
+    //Public section
     public:
-        string To;
-        string From;
 
+
+        //This is the section where when you make a plane object, this stuff happens
         plane(string from, string to)
         {
+            //Sets the to and from to the origion and destination
             origin = from;
             destination = to;
         }
 
+        //This is the deconstructor
         ~plane()
         {
             cout<<"Deleted Plane Object"<<endl;
         }
 
+        //The oporate function
         void oporate(double dt)
         {
             
         }
 
+        //Gets the position
         double get_pos()
         {
             return pos;
         }
 
+        //Gets the origin
         string get_origin()
         {
             return origin;
         }
 
+        //Gets the destination
         string get_destination(){
             return destination;
         }
 
+        //Gets if we are at SCE
         bool get_SCE()
         {
             return at_SCE;
         }
 
+        //Gets the velocity
         double get_vel()
         {
             return vel;
         }
 
+        //Sets the velocity
         void set_vel(double New_Velocity)
         {
             vel = New_Velocity;
         }
+
+    //Private Section
     private:
+
+        //Sets all of the variables and makes a new Airport rout
         double pos;
         double vel;
         double distance;
@@ -71,6 +88,7 @@ class plane
     
 };
 
+//Main
 int main()
 {
 
